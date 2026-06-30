addbe19206565b633349e772392511c8382c08f4
maksbuk <bukatinmaksimilian585@gmail.com>
2026-06-30 13:17:38 +0300

    Initial Commit

---
+++ DMB timerTests/DMB_timerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DMB_timerTests.swift
+//  DMB timerTests
+//
+//  Created by  BOSS on 30.06.26.
+//
+
+import Testing
+@testable import DMB_timer
+
+struct DMB_timerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ DMB timerUITests/DMB_timerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DMB_timerUITestsLaunchTests.swift
+//  DMB timerUITests
+//
+//  Created by  BOSS on 30.06.26.
+//
+
+import XCTest
+
+final class DMB_timerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
